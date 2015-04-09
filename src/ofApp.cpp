52833c479c72255c4e305c25e42593e57fc131c7
eduardfrigola <eduardfrigola@yahoo.com>
2015-04-09 19:07:02 +0200

    "initial commit"

---
+++ src/ofApp.cpp
@@ -0,0 +1,257 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    buttonWidth=200;
+    buttonHeight=100;
+    
+    buttonPosition.x=ofGetWidth()/2-buttonWidth/2;
+    buttonPosition.y=ofGetHeight()/1.15-buttonHeight/2;
+    
+    buttonColor.r=0;
+    buttonColor.g=0;
+    buttonColor.b=100;
+    
+    bike.loadImage("bike_no_pedals.png");
+    
+    bodyColor=ofColor(239, 200, 200);
+    
+    beginX=170;
+    endX=ofGetWidth()-350;
+    bikerOrigin=ofPoint(beginX, ofGetHeight()/2);
+    
+    kneeLPosition=ofPoint(bikerOrigin.x+70,bikerOrigin.y);
+    kneeRPosition=ofPoint(bikerOrigin.x+70,bikerOrigin.y);
+    
+    pedalRadius=34;
+    
+    zerotopi=0;
+    
+    play=false;
+    stopstate=true;
+    speed=0;
+    
+    trafficLightPos=ofPoint(900, 20);
+    redLightColor=ofColor(255,0,0,50);
+    orangeLightColor=ofColor(255,145,0, 255);
+    greenLightColor=ofColor(0,255,0, 50);
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    //variables that have to update
+    kneeLPosition.x=bikerOrigin.x+70;
+    kneeRPosition.x=bikerOrigin.x+70;
+    pedalierCenter=ofPoint(bikerOrigin.x+67, bikerOrigin.y+92);
+    
+    
+    //Set and reset initial state
+    if (stopstate==true){
+        bikerOrigin=ofPoint(170, ofGetHeight()/2);
+        zerotopi=0;
+        
+        pedalierCenter=ofPoint(bikerOrigin.x+67, bikerOrigin.y+92);
+        pedalRPosition.x=pedalRadius*cos(zerotopi)+pedalierCenter.x;
+        pedalRPosition.y=pedalRadius*sin(zerotopi)+pedalierCenter.y;
+        
+        pedalLPosition.x=-pedalRadius*cos(zerotopi)+pedalierCenter.x;
+        pedalLPosition.y=-pedalRadius*sin(zerotopi)+pedalierCenter.y;
+        
+        kneeLPosition=ofPoint(bikerOrigin.x+70,bikerOrigin.y);
+        kneeRPosition=ofPoint(bikerOrigin.x+70,bikerOrigin.y);
+        speed=0;
+        stopstate=false; //only has to run one time;
+    }
+    
+    
+    
+    //forward movement
+    if(bikerOrigin.x<=endX && play==true){
+        bikerOrigin.x+=speed;
+        speed+=0.05;
+        zerotopi+=0.05;
+        if (zerotopi>=2*PI){
+            zerotopi=0;
+        }
+        pedalRPosition.x=pedalRadius*cos(zerotopi)+pedalierCenter.x;
+        pedalRPosition.y=pedalRadius*sin(zerotopi)+pedalierCenter.y;
+    
+        pedalLPosition.x=-pedalRadius*cos(zerotopi)+pedalierCenter.x;
+        pedalLPosition.y=-pedalRadius*sin(zerotopi)+pedalierCenter.y;
+    
+        kneeRPosition.y+=cos(zerotopi);
+        kneeLPosition.y+=-cos(zerotopi);
+    }
+    
+
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    ofDrawBitmapString("Speed: "+ofToString(speed)+" px/frame", ofPoint(5,20));
+    
+    ofSetColor(0);
+    ofRect(900, 20, 80, 220);
+    ofSetColor(redLightColor);
+    ofCircle(trafficLightPos.x+40, trafficLightPos.y+40, 30);
+    ofSetColor(orangeLightColor);
+    ofCircle(trafficLightPos.x+40, trafficLightPos.y+110, 30);
+    ofSetColor(greenLightColor);
+    ofCircle(trafficLightPos.x+40, trafficLightPos.y+180, 30);
+    
+    
+    //widht of lines
+    ofSetLineWidth(12);
+    
+    
+    
+    
+    ofSetLineWidth(12);
+    
+    //floor
+    ofRect( 0, 550, ofGetWidth(),10);
+
+    
+    
+    //head
+    ofCircle(bikerOrigin.x, bikerOrigin.y-300, 30);
+
+    //leftarm
+    ofSetColor(bodyColor.r+30, bodyColor.g, bodyColor.b);
+    ofLine(bikerOrigin.x, bikerOrigin.y-240, bikerOrigin.x+125, bikerOrigin.y-140);
+    
+    //right hand
+    ofCircle(bikerOrigin.x+125, bikerOrigin.y-140, 10);
+    
+    //left cuixa
+    ofLine(ofPoint(bikerOrigin.x, bikerOrigin.y-100), kneeLPosition);
+   
+    
+    //right cama
+    ofLine(kneeLPosition, pedalLPosition);
+    
+    //draw pedal arm
+    ofSetColor(70);
+    ofLine(pedalierCenter, pedalLPosition);
+    
+    
+    //knee joint
+    ofSetColor(bodyColor.r+30, bodyColor.g, bodyColor.b);
+    ofCircle(kneeLPosition, 7);
+    
+    //draw pedal
+    ofSetColor(255, 0, 0);
+    ofRect(pedalLPosition.x-10, pedalLPosition.y-2.5, 20, 5);
+
+    //bike
+    ofSetColor(255);
+    bike.draw(bikerOrigin.x-bike.width/2+90, bikerOrigin.y-bike.height/2+20);
+    
+    //---------------------------------------------------------------------------------------------
+    //things draw top of bike
+    
+    //body and head
+    ofSetColor(bodyColor.r+60, bodyColor.g, bodyColor.b);
+    ofCircle(bikerOrigin.x, bikerOrigin.y-300, 30);
+    ofEllipse(bikerOrigin.x, bikerOrigin.y-170, 100, 200);
+    
+    //right arm
+    ofSetColor(bodyColor);
+    ofLine(bikerOrigin.x, bikerOrigin.y-240, bikerOrigin.x+120, bikerOrigin.y-135);
+    
+    //arm joint
+    ofCircle(bikerOrigin.x, bikerOrigin.y-240, 10);
+    
+    //right hand
+    ofCircle(bikerOrigin.x+120, bikerOrigin.y-135, 10);
+    
+    //right cuixa
+    ofLine(ofPoint(bikerOrigin.x, bikerOrigin.y-100), kneeRPosition);
+    
+    //pelvis
+    ofCircle(bikerOrigin.x, bikerOrigin.y-100, 12);
+    
+    //draw pedal arm
+    ofSetColor(70);
+    ofLine(pedalierCenter, pedalRPosition);
+    
+    //center of pedalier
+    ofCircle(pedalierCenter, 9);
+    
+    //right cama
+    ofSetColor(bodyColor);
+    ofLine(kneeRPosition, pedalRPosition);
+    
+    //knee joint
+    ofCircle(kneeRPosition, 7);
+    
+    //draw pedal
+    ofSetColor(255, 0, 0);
+    ofRect(pedalRPosition.x-10, pedalRPosition.y-2.5, 20, 5);
+
+    
+    //draw button
+    ofSetColor(buttonColor);
+    ofRect(buttonPosition,buttonWidth,buttonHeight);
+    
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    play=false;
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    play=true;
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    mousePosition.x=x;
+    mousePosition.y=y;
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button)
+{
+    if(ofInRange(x, buttonPosition.x, buttonPosition.x+buttonWidth) && ofInRange(y, buttonPosition.y, buttonPosition.y+buttonHeight))
+    {
+        buttonColor = ofColor(0,255,0);
+    }
+    
+    stopstate=true;
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+  //prova
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+//prova2
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/ofApp.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    private:
+        ofPoint mousePosition;
+    
+    ofPoint buttonPosition;
+    int buttonWidth;
+    int buttonHeight;
+    ofColor buttonColor;
+    
+    ofColor bodyColor;
+    
+    ofPoint bikerOrigin;
+    
+    ofPoint kneeLPosition;
+    ofPoint kneeRPosition;
+    
+    ofPoint pedalierCenter;
+    ofPoint pedalLPosition;
+    ofPoint pedalRPosition;
+    int pedalRadius;
+    float zerotopi;
+    
+    ofImage bike;
+    
+    bool stopstate;
+    bool play;
+    
+    int beginX;
+    int endX;
+    
+    float speed;
+    
+    ofColor redLightColor;
+    ofColor greenLightColor;
+    ofColor orangeLightColor;
+    
+    ofPoint trafficLightPos;
+};
